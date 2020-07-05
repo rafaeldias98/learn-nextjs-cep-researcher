a4107d9ed335216905d904e085b24e2c00ef3ef4
Rafael Dias <rafadias-98@hotmail.com>
2020-07-05 00:59:02 -0300

    Configure e2e tests folder

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -6,7 +6,7 @@ loadServices();
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ["tests/e2e"],
+  src_folders: ['tests/e2e'],
 
   // See https://nightwatchjs.org/guide/working-with-page-objects/
   page_objects_path: '',
